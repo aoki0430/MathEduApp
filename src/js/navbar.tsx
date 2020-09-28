31ebf255c6bc593b21ec10bcbc62ccb74fc8ac49
aoki0430 <31432520+garuda02@users.noreply.github.com>
2020-09-28 23:37:47 +0900

    test anim lottie

--- src/js/navbar.tsx
+++ src/js/navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Route, Link } from 'react-router-dom'
+import {Route, Link } from 'react-router-dom';
+import Electron from '../animation/electron'
 // :: 三平方の定理，ピタゴラスの定理
 // :: Pythagorean Theorem
 
@@ -12,8 +13,9 @@ export default class Navbar extends React.Component{
                 <p>powered by uji, hatta, aoki</p>
                 <p><Link to="/">this is home</Link></p>
                 <p><Link to="/Pytha1">this is pytha1</Link></p>
+                <h2>Electron test</h2>
+                <Electron />
             </div>
         );
-        
     }
 }
